0046fd429169e566ad64eefe2a5f9b23a0490a65
Dina <maskaykinadina@gmail.com>
2022-03-28 00:37:57 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // входные данные
+        int bonus = 20;
+        int priceTicket = 1000;
+
+        // логика
+        System.out.println("количство начисленных миль:");
+        System.out.println(priceTicket/bonus);
+}
+}
+
